0782f0c214e66e5f5ecb7084b281172bdb35765f
toyrik <toyrik@gmail.com>
2020-09-06 18:19:23 +0300

    Реализуем механизм миграций

---
+++ migrations/m0002_initial.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of m0002_initial
+ *
+ * @author toyrik
+ */
+class m0002_initial
+{
+    public function up()
+    {
+        echo 'Applying migration...'.PHP_EOL;
+    }
+    
+    public function down()
+    {
+        echo 'Down migration...'.PHP_EOL;
+    }
+}
