187a95c4a83a2119438912fb376ac45d6d8e70d9
toyrik <toyrik@gmail.com>
2020-09-06 20:35:24 +0300

    Добавляем правило проверки уникальности поля

--- core/Database.php
+++ core/Database.php
@@ -72,6 +72,11 @@ class Database
         $statement->execute();
     }
     
+    public function prepare($sql)
+    {
+        return $this->pdo->prepare($sql);
+    }
+    
     private function log($message)
     {
         echo "[" . date("Y-m-d H:i:s") . "] - " . $message . PHP_EOL;
